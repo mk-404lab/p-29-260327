0e2326d502b6e7d0f3b36f4651415c519fbd1024
mk-404lab <alsgur9984@naver.com>
2026-04-05 19:03:35 +0900

    feat: 45강 - 헤더 방식의 인증 정보가 없을 경우, 쿠키 방식으로 인증 처리 가능하도록 구현

--- src/main/java/com/back/global/rq/Rq.java
+++ src/main/java/com/back/global/rq/Rq.java
@@ -9,6 +9,8 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
+
 @Component
 @RequiredArgsConstructor
 public class Rq {
@@ -29,16 +31,39 @@ public class Rq {
 
     public Member getActor() {
         String authorizationHeader = request.getHeader("Authorization");
+        String apiKey;
 
-        if (authorizationHeader == null) {
-            throw new ServiceException("401-1", "인증 정보가 헤더에 존재하지 않습니다.");
+//        if (authorizationHeader == null) {
+//            throw new ServiceException("401-1", "인증 정보가 헤더에 존재하지 않습니다.");
+//        }
+//
+//        if (!authorizationHeader.startsWith("Bearer ")) {
+//            throw new ServiceException("401-2", "잘못된 형식의 인증 데이터입니다.");
+//        }
+//
+//        String apiKey = authorizationHeader.replace("Bearer ", "");
+
+
+        if (authorizationHeader != null) {
+        // 1번 방식 : 요청 헤더에서 authorization 인증 정보를 찾음
+
+            if (!authorizationHeader.startsWith("Bearer ")) {
+                throw new ServiceException("401-2", "잘못된 형식의 인증 데이터입니다.");
+            }
+            apiKey = authorizationHeader.replace("Bearer ", "");
+        } else {
+        // 2번 방식 : 헤더에 인증 정보가 없을 경우, 쿠키에서 인증 정보를 찾음
+
+            apiKey = request.getCookies() == null ? "" : Arrays.stream(request.getCookies())
+                    .filter(cookie -> cookie.getName().equals("apiKey"))
+                    .map(Cookie::getValue)
+                    .findFirst()
+                    .orElse("");
         }
 
-        if (!authorizationHeader.startsWith("Bearer ")) {
-            throw new ServiceException("401-2", "잘못된 형식의 인증 데이터입니다.");
+        if (apiKey.isBlank()) {
+            throw new ServiceException("401-3", "인증 정보가 존재하지 않습니다.");
         }
-
-        String apiKey = authorizationHeader.replace("Bearer ", "");
 
         return memberService.findByApiKey(apiKey).orElseThrow(
                 () -> new ServiceException("401-1", "유효하지 않은 API 키입니다.")
