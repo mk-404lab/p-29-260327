7cd6f2cc0b616cb0028c8f82460b41c97ff1a688
mk-404lab <alsgur9984@naver.com>
2026-04-09 01:12:34 +0900

    feat: 61강 - accessToken 우선 사용 / apiKey는 반드시 존재, accessToken은 선택

--- src/main/java/com/back/global/rq/Rq.java
+++ src/main/java/com/back/global/rq/Rq.java
@@ -10,6 +10,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
+import java.util.Map;
+import java.util.Optional;
 
 @Component
 @RequiredArgsConstructor
@@ -19,44 +21,77 @@ public class Rq {
     private final HttpServletRequest request;
     private final HttpServletResponse response;
 
-    public void addCookie(String name, String value) {
 
-        Cookie cookie = new Cookie(name, value);
-        cookie.setPath("/");
-        cookie.setHttpOnly(true);
-        cookie.setDomain("localhost");
-
-        response.addCookie(cookie);
-    }
 
     public Member getActor() {
-        String authorizationHeader = request.getHeader("Authorization");
-        String apiKey;
+        String authorizationHeader = getHeader("Authorization", "");
 
-        if (authorizationHeader != null) {
-        // 1번 방식 : 요청 헤더에서 authorization 인증 정보를 찾음
+        String apiKey;
+        String accessToken;
+
+        if (!authorizationHeader.isBlank()) {
+            // 1번 방식 : 요청 헤더에서 authorization 인증 정보를 찾음
 
             if (!authorizationHeader.startsWith("Bearer ")) {
                 throw new ServiceException("401-2", "잘못된 형식의 인증 데이터입니다.");
             }
-            apiKey = authorizationHeader.replace("Bearer ", "");
-        } else {
-        // 2번 방식 : 헤더에 인증 정보가 없을 경우, 쿠키에서 인증 정보를 찾음
 
-            apiKey = request.getCookies() == null ? "" : Arrays.stream(request.getCookies())
-                    .filter(cookie -> cookie.getName().equals("apiKey"))
-                    .map(Cookie::getValue)
-                    .findFirst()
-                    .orElse("");
+            String[] headerAuthoizationBits = authorizationHeader.split(" ", 3);
+//            apiKey = authorizationHeader.replace("Beare ", "");
+
+            apiKey = headerAuthoizationBits[1];
+            accessToken = headerAuthoizationBits.length == 3 ? headerAuthoizationBits[2] : "";
+        } else {
+            // 2번 방식 : 쿠키
+            apiKey = getCookieValue("apiKey", "");
+            accessToken = getCookieValue("accessToken", "");
         }
+
+        Member member = null;
 
         if (apiKey.isBlank()) {
-            throw new ServiceException("401-3", "인증 정보가 존재하지 않습니다.");
+            throw new ServiceException("401-1", "apiKey가 존재하지 않습니다.");
         }
 
-        return memberService.findByApiKey(apiKey).orElseThrow(
-                () -> new ServiceException("401-1", "유효하지 않은 API 키입니다.")
-        );
+        if (!accessToken.isBlank()) {
+            Map<String, Object> payload = memberService.payloadOrNull(accessToken);
+
+            if (payload != null) {
+                int id = (int) payload.get("id");
+                member = memberService.findById(id)
+                        .orElseThrow(() -> new ServiceException("401-3", "accessToken의 id에 해당하는 회원이 존재하지 않습니다."));
+            }
+        }
+
+        // accessToken으로 인증이 제대로 이루어지지 않은 경우
+        if (member == null) {
+            member = memberService
+                    .findByApiKey(apiKey)
+                    .orElseThrow(() -> new ServiceException("401-4", "API 키가 유효하지 않습니다."));
+        }
+
+        return member;
+    }
+
+    private String getHeader(String name, String defaultValue) {
+        return Optional
+                .ofNullable(request.getHeader(name))
+                .filter(headerValue -> !headerValue.isBlank())
+                .orElse(defaultValue);
+    }
+
+    private String getCookieValue(String name, String defaultValue) {
+        return Optional
+                .ofNullable(request.getCookies())
+                .flatMap(
+                        cookies ->
+                                Arrays.stream(cookies)
+                                        .filter(cookie -> cookie.getName().equals(name))
+                                        .map(Cookie::getValue)
+                                        .filter(value -> !value.isBlank())
+                                        .findFirst()
+                )
+                .orElse(defaultValue);
     }
 
     public void deleteCookie(String name) {
@@ -65,6 +100,16 @@ public class Rq {
         cookie.setHttpOnly(true);
         cookie.setDomain("localhost");
         cookie.setMaxAge(0);
+
+        response.addCookie(cookie);
+    }
+
+    public void addCookie(String name, String value) {
+
+        Cookie cookie = new Cookie(name, value);
+        cookie.setPath("/");
+        cookie.setHttpOnly(true);
+        cookie.setDomain("localhost");
 
         response.addCookie(cookie);
     }
--- src/test/java/com/back/domain/post/post/controller/ApiV1PostControllerTest.java
+++ src/test/java/com/back/domain/post/post/controller/ApiV1PostControllerTest.java
@@ -175,7 +175,7 @@ public class ApiV1PostControllerTest {
                 .andExpect(handler().methodName("write"))
                 .andExpect(status().isUnauthorized())
                 .andExpect(jsonPath("$.resultCode").value("401-1"))
-                .andExpect(jsonPath("$.msg").value("인증 정보가 헤더에 존재하지 않습니다."));
+                .andExpect(jsonPath("$.msg").value("apiKey가 존재하지 않습니다."));
     }
 
     @Test
@@ -357,8 +357,8 @@ public class ApiV1PostControllerTest {
                 .andExpect(handler().handlerType(ApiV1PostController.class))
                 .andExpect(handler().methodName("write"))
                 .andExpect(status().isUnauthorized())
-                .andExpect(jsonPath("$.resultCode").value("401-1"))
-                .andExpect(jsonPath("$.msg").value("유효하지 않은 API 키입니다."));
+                .andExpect(jsonPath("$.resultCode").value("401-4"))
+                .andExpect(jsonPath("$.msg").value("API 키가 유효하지 않습니다."));
     }
 
     @Test
