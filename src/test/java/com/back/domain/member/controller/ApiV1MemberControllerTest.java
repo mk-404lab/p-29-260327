3f14b33d8b4941cc19e0e76ee1301f383aa1beb7
mk-404lab <alsgur9984@naver.com>
2026-04-08 12:20:08 +0900

    feat: 60강 - MemberController에서 accessToken 발급 구현

--- src/test/java/com/back/domain/member/controller/ApiV1MemberControllerTest.java
+++ src/test/java/com/back/domain/member/controller/ApiV1MemberControllerTest.java
@@ -128,7 +128,9 @@ public class ApiV1MemberControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.resultCode").value("200-1"))
                 .andExpect(jsonPath("$.msg").value("%s님 환영합니다.".formatted(member.getNickname())))
-                .andExpect(jsonPath("$.data.apiKey").exists());
+                .andExpect(jsonPath("$.data.apiKey").exists())
+                .andExpect(jsonPath("$.data.accessToken").isNotEmpty());
+
 
         resultActions.andExpect(
                 result -> {
@@ -141,6 +143,15 @@ public class ApiV1MemberControllerTest {
                     assertThat(apiKeyCookie.isHttpOnly()).isTrue();
 
                     assertThat(apiKeyCookie.getDomain()).isEqualTo("localhost");
+
+
+                    Cookie accessTokenCookie = result.getResponse().getCookie("accessToken");
+                    assertThat(accessTokenCookie).isNotNull();
+
+                    assertThat(accessTokenCookie.getPath()).isEqualTo("/");
+                    assertThat(accessTokenCookie.getDomain()).isEqualTo("localhost");
+                    assertThat(accessTokenCookie.isHttpOnly()).isEqualTo(true);
+
                 }
         );
     }
@@ -191,8 +202,8 @@ public class ApiV1MemberControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.id").value(member.getId()))
                 .andExpect(jsonPath("$.name").value(member.getName()))
-                .andExpect(jsonPath("$.createDate").value(member.getCreateDate().toString()))
-                .andExpect(jsonPath("$.modifyDate").value(member.getModifyDate().toString()));
+                .andExpect(jsonPath("$.createDate").exists())
+                .andExpect(jsonPath("$.modifyDate").exists());
 
     }
 }
